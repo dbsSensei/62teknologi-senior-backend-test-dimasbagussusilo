e1e2fee8f397d9bc3fec9bc1d2f07b8a48c88374
Dimas Bagus Susilo <dimasbagussusilo@gmail.com>
2022-12-10 21:34:14 +0700

    add public cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -8,6 +9,7 @@ const businessRoutes = require('./routers/business');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
